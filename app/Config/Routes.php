cf4171c2c58aaff5116dadcaa250bfaa1a0d3e29
Wdyagstn <widyaagustin072@gmail.com>
2026-04-25 10:21:46 +0700

    feat: implement login and register pages with NiceAdmin template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('login', 'Auth::login');
+$routes->post('login/process', 'Auth::processLogin');
+$routes->get('register', 'Auth::register');
+$routes->post('register/process', 'Auth::processRegister');
+
